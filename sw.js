eb63bfdc17567613d8e68020e113e835ed6974b5
CedricT33 <cedric.tressous@gmail.com>
2020-10-03 18:24:13 +0200

    changement de version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Constantes
-const VERSION = "01.00.006";
+const VERSION = "01.00.007";
 const CACHE_NAME_STATIC = "static-" + VERSION;
 const CACHE_NAME_DYNAMIC = "dynamic-" + VERSION;
 const urlsToCache = [ 
